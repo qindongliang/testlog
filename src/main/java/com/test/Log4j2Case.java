37665c0a0c45c94853b0a4f242160628273ca861
qindongliang <18210507492@126.com>
2019-09-22 00:04:53 +0800

    refactor class name

--- src/main/java/com/test/Log4j2Case.java
+++ src/main/java/com/test/Log4j2Case.java
@@ -3,7 +3,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Log4j2Case {
+
 static final Logger log = LogManager.getLogger(Log4j2Case.class);
+
     public static void main(String[] args) {
         System.out.println(123);
         log.info("123");
