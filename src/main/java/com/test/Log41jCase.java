c57ed2404d99054a8a2bef95b9d2eb9869b7c27b
qindongliang <18210507492@126.com>
2019-09-21 23:58:29 +0800

    refactor package and class name

--- src/main/java/com/test/Log41jCase.java
+++ src/main/java/com/test/Log41jCase.java
@@ -1,9 +1,9 @@
-package com.dong;
+package com.test;
 
 import org.apache.log4j.Logger;
 
-public class Log4j1Case {
-    static final Logger log = Logger.getLogger(Log4j1Case.class);
+public class Log41jCase {
+    static final Logger log = Logger.getLogger(Log41jCase.class);
     public static void main(String[] args) {
         log.info("使用log4j 1.x 打印log");
         System.out.println("log4j 1.x");
